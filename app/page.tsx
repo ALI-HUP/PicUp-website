616ca9600c21ae13d335db9495c57b503ef84db0
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-01-16 15:31:35 +0330

    update on header

--- app/page.tsx
+++ app/page.tsx
@@ -9,18 +9,10 @@ const Home = () => {
         </div>
 
         <div className="text-xl space-x-10 p-3 ml-auto">
-          <a href="/headers/photos">
-            Photos
-          </a>
-          <a href="/headers/new-ones">
-            New Ones
-          </a>
-          <a href="/headers/about">
-            About
-          </a>
-          <a href="/headers/saved">
-            Saved
-          </a>
+          <a href="/headers/photos" className="underline-animation hover:font-medium"> Photos </a>
+          <a href="/headers/new-ones" className="underline-animation hover:font-medium"> New Ones </a>
+          <a href="/headers/about" className="underline-animation hover:font-medium"> About </a>
+          <a href="/headers/saved" className="underline-animation hover:font-medium"> Saved </a>
         </div>
       </header>
     </div>
