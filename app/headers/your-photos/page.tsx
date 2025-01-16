4c8e2f9c6191182f8c9da53a31e0f669c3e8f2a6
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-01-16 15:47:26 +0330

    new updates on header

--- app/headers/your-photos/page.tsx
+++ app/headers/your-photos/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Photos = () => {
+const YourPhotos = () => {
   return (
     <div>
-      <h2>Photos</h2>
+      <h2>Your Photos</h2>
       <p>Here are all the photos...</p>
     </div>
   );
 };
 
-export default Photos;
+export default YourPhotos;
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <header className="p-3 flex items-center bg-slate-600">
+      <header className="p-3 flex items-center bg-slate-600 mb-20 ">
         <div>
           <h1 className="text-3xl p-2 font-extrabold">PicUp Website</h1>
         </div>
 
         <div className="text-xl space-x-10 p-3 ml-auto">
-          <a href="/headers/photos" className="underline-animation hover:font-medium"> Photos </a>
-          <a href="/headers/new-ones" className="underline-animation hover:font-medium"> New Ones </a>
-          <a href="/headers/about" className="underline-animation hover:font-medium"> About </a>
-          <a href="/headers/saved" className="underline-animation hover:font-medium"> Saved </a>
+          <a href="/headers/your-photos" className="underline-animation hover:font-medium">Your Photos</a>
+          <a href="/headers/new-ones" className="underline-animation hover:font-medium">New Ones</a>
+          <a href="/headers/about" className="underline-animation hover:font-medium">About</a>
+          <a href="/headers/saved" className="underline-animation hover:font-medium">Saved</a>
         </div>
       </header>
     </div>
