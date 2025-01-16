fa1c7ad4f96bf7585be501afd88f8abd2bd69dee
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-01-16 14:30:28 +0330

    cleaning up and adding the first version of header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import React from "react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sequi. Quidem dolor veritatis. </p>
+      <header className="p-3 flex items-center bg-slate-600">
+        <div>
+          <h1 className="text-3xl p-2 font-extrabold">PicUp Website</h1>
+        </div>
+
+        <div className="text-xl space-x-10 p-2 ml-auto">
+          <span>Photos</span>
+          <span>New Ones</span>
+          <span>About</span>
+          <span>Saved</span>
+        </div>
+      </header>
     </div>
   );
 }
