d005d94b7ae3eeb90b4d2390b76d265effa9ef33
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-01-16 16:03:02 +0330

    add pictures and a frame

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Image from "next/image";
+import Pics from "@/public/pics/IMG_20220307_152910_643.jpg";
 
 const Home = () => {
   return (
-    <div>
+    <div className="mb-52">
       <header className="p-3 flex items-center bg-slate-600 mb-20 ">
         <div>
           <h1 className="text-3xl p-2 font-extrabold">PicUp Website</h1>
@@ -15,6 +17,37 @@ const Home = () => {
           <a href="/headers/saved" className="underline-animation hover:font-medium">Saved</a>
         </div>
       </header>
+
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-3 gap-10 w-full max-w-[1200px]">
+          <div className="w-[335px] h-[390px] bg-white flex justify-center">
+            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          </div>
+          <div className="w-[335px] h-[390px] bg-white flex justify-center">
+            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          </div>
+          <div className="w-[335px] h-[390px] bg-white flex justify-center">
+            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          </div>
+
+          <div className="w-[335px] h-[390px] bg-white flex justify-center">
+            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          </div>
+          <div className="w-[335px] h-[390px] bg-white flex justify-center">
+            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          </div>
+          <div className="w-[335px] h-[390px] bg-white flex justify-center">
+            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          </div>
+
+          <div className="w-[335px] h-[390px] bg-white flex justify-center">
+            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          </div>
+          <div className="w-[335px] h-[390px] bg-white flex justify-center">
+            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
