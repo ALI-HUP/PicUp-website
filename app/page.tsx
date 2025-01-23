7e3404dddfd8aac7f30bedaadbfa11318e195ced
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-01-23 23:25:53 +0330

    first arrange for layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
-import Pics from "@/public/pics/IMG_20220307_152910_643.jpg";
+import Pics1 from "@/public/pics/IMG_20220307_152910_643.jpg";
+import Pics2 from "@/public/pics/wp7864479.png";
+import Pics3 from "@/public/pics/wp7937871.jpg";
 import Love from "@/public/svg/heart-svgrepo-com.png";
 import Share from "@/public/svg/share-svgrepo-com.png";
 import Saved from "@/public/svg/saved-svgrepo-com.png";
@@ -21,31 +23,64 @@ const Home = () => {
         </div>
       </header>
 
-      <div className="flex justify-center items-center">
-        <div className="grid grid-cols-3 gap-10 w-full max-w-[1200px]">
-          <div className="w-[335px] h-[390px] bg-white flex justify-center relative">
-            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
-            
-            <div className="absolute bottom-4 right-1 flex gap-5">
-              <Image src={Love} alt="love" className="w-7 h-7 hover:scale-110 transition-all duration-150" />
-              <Image src={Share} alt="share" className="w-7 h-7 hover:scale-110 transition-all duration-150" />
-              <Image src={Saved} alt="saved" className="w-[30px] h-[30px] hover:scale-110 transition-all duration-150" />
-            </div>
-
-            <div className="absolute bottom-2 left-2">
-              <button className="bg-blue-600 text-white py-2 px-5 hover:bg-blue-700 hover:rounded-md duration-100">
-                Download
-              </button>
-            </div>
+      <div className="flex flex-wrap justify-start gap-8 w-full max-w-[1400px] m-auto">
+        <div className="relative bg-white flex justify-center items-center p-1">
+          <Image
+            src={Pics1}
+            alt="car pic"
+            className="p-2 pb-16"
+          />
+          <div className="absolute bottom-4 right-1 flex gap-5">
+            <Image src={Love} alt="love" className="w-7 h-7 hover:scale-110 transition-all duration-150" />
+            <Image src={Share} alt="share" className="w-7 h-7 hover:scale-110 transition-all duration-150" />
+            <Image src={Saved} alt="saved" className="w-[30px] h-[30px] hover:scale-110 transition-all duration-150" />
           </div>
 
-          <div className="w-[335px] h-[390px] bg-white flex justify-center">
-            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
-          </div>
-          <div className="w-[335px] h-[390px] bg-white flex justify-center">
-            <Image src={Pics} alt="car pic" className="w-80 h-80 mt-2" />
+          <div className="absolute bottom-2 left-2">
+            <button className="bg-blue-600 text-white py-2 px-5 hover:bg-blue-700 hover:rounded-md duration-100">
+              Download
+            </button>
           </div>
         </div>
+
+        <div className="relative bg-white flex justify-center items-center p-1">
+         <Image
+            src={Pics2}
+            alt="car pic"
+            className="p-2 pb-16"
+          />
+          <div className="absolute bottom-4 right-1 flex gap-5">
+            <Image src={Love} alt="love" className="w-7 h-7 hover:scale-110 transition-all duration-150" />
+            <Image src={Share} alt="share" className="w-7 h-7 hover:scale-110 transition-all duration-150" />
+            <Image src={Saved} alt="saved" className="w-[30px] h-[30px] hover:scale-110 transition-all duration-150" />
+          </div>
+
+          <div className="absolute bottom-2 left-2">
+            <button className="bg-blue-600 text-white py-2 px-5 hover:bg-blue-700 hover:rounded-md duration-100">
+              Download
+            </button>
+          </div>
+        </div>
+
+        <div className="relative bg-white flex justify-center items-center">
+          <Image
+            src={Pics3}
+            alt="car pic"
+            className=" p-2 pb-16"
+          />
+          <div className="absolute bottom-4 right-1 flex gap-5">
+            <Image src={Love} alt="love" className="w-7 h-7 hover:scale-110 transition-all duration-150" />
+            <Image src={Share} alt="share" className="w-7 h-7 hover:scale-110 transition-all duration-150" />
+            <Image src={Saved} alt="saved" className="w-[30px] h-[30px] hover:scale-110 transition-all duration-150" />
+          </div>
+
+          <div className="absolute bottom-2 left-2">
+            <button className="bg-blue-600 text-white py-2 px-5 hover:bg-blue-700 hover:rounded-md duration-100">
+              Download
+            </button>
+          </div>
+        </div>
+
       </div>
     </div>
   );
