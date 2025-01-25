00d01b069d6667e80dcaf5ee1ae6787cb46b37b1
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-01-25 22:04:22 +0330

    add header to all pages

--- app/headers/about/page.tsx
+++ app/headers/about/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from '@/components/Header';
 
 const About = () => {
   return (
     <div>
+
+      <Header />
+
       <h2>About</h2>
       <p>About the PicUp website...</p>
     </div>
--- app/headers/saved/page.tsx
+++ app/headers/saved/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import Header from '@/components/Header';
 
 const Saved = () => {
   return (
     <div>
+
+      <Header />
+
       <h2>Saved</h2>
       <p>Here are your saved items...</p>
     </div>
