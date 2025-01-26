11c091cf579adbbcdf0956322cdf24489268b998
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-01-26 23:33:19 +0330

    add a small about text

--- app/headers/about/page.tsx
+++ app/headers/about/page.tsx
@@ -4,11 +4,12 @@ import Header from '@/components/Header';
 const About = () => {
   return (
     <div>
-
       <Header />
 
       <h2>About</h2>
-      <p>About the PicUp website...</p>
+      <br /><br />
+      <p>This website is created for photographers.</p>
+      <p>This is a platform to share your photos with others and publish them world wide</p>
     </div>
   );
 };
