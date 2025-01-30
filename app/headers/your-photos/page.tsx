f827daee2b349188d26e3541158851a2a39a18ef
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-01-30 17:22:22 +0330

    add photo counter

--- app/headers/your-photos/page.tsx
+++ app/headers/your-photos/page.tsx
@@ -18,24 +18,21 @@ const YourPhotos = () => {
   return (
     <div className="mb-40">
       <Header />
-
-      <div className="flex justify-center items-center text-center p-8 bg-slate-500 text-2xl font-extrabold">
-        <h1 className="w-[50%]">Here are your photos</h1>
+      
+      <div className="w-[70%] m-auto mt-10 rounded-full bg-slate-400">
+        <div className="flex justify-center items-center text-center p-3 text-2xl font-bold">
+          <h1 className="w-[50%]">Here are your photos</h1>
+          <h1 className="w-[50%]">Photos: {uploadedImages.length}</h1>
+        </div>
       </div>
-
-      <div className="flex justify-center items-start grid-cols-3 gap-8 max-w-[1400px] m-auto mt-10">
-        {uploadedImages.length > 0 ? (
-          uploadedImages.map((image, index) => (
-            <div key={index} className="bg-white">
-              <Image src={image as string} alt={`photo ${index}`} width={500} height={500} className="object-cover" />
-              <IconsAndButton downloadLink={`https://your-website-link.com/${index + 1}`} />
-            </div>
-          ))
-        ) : (
-          <div className="w-full text-center">
-            <h2>No photos uploaded yet</h2>
+      
+      <div className="flex flex-wrap justify-center gap-5 mt-10">
+        {uploadedImages.map((image, index) => (
+          <div key={index}>
+            <Image src={image as string} alt={`photo ${index}`} width={500} height={500} />
+            <IconsAndButton downloadLink={`https://your-website-link.com/${index + 1}`} />
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
