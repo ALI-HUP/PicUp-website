42f868596b25d1d098562deb2cac55c09f50c9da
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-02-01 15:25:41 +0330

    first stage of improving header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,24 +3,29 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="p-3 flex items-center bg-slate-600">
-      <div>
-        <Link href="/" className="flex text-3xl p-2 font-extrabold">
-          <h1>PicUp Website</h1>
-        </Link>
-      </div>
+    <header className="flex text-xl p-3 bg-slate-600">
+      
+        <div>
+          <Link href="/" className="flex text-3xl p-2 font-extrabold">
+            <h1>PicUp Website</h1>
+          </Link>
+        </div>
 
-      <div className="text-xl space-x-10 p-3 ml-auto">
-        <Link href="/headers/upload" className="underline-animation">
-          Upload
-        </Link>
-        <Link href="/headers/saved" className="underline-animation">
-          Saved
-        </Link>
-        <Link href="/headers/profile" className="underline-animation">
-          Profile
-        </Link>
-      </div>
+        <div className="">
+          <Link href="/headers/upload" className="underline-animation">
+            Upload
+          </Link>
+          <Link href="/headers/saved" className="underline-animation">
+            Saved
+          </Link>
+        </div>
+     
+        <div>
+          <Link href="/headers/profile" className="underline-animation">
+            Profile
+          </Link>
+        </div>
+     
     </header>
   );
 };
