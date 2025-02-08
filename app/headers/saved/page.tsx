64ab9424c14b07beadf904f286da894ebb5763bb
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-02-08 20:28:36 +0330

    first stage of having saved page

--- app/headers/saved/page.tsx
+++ app/headers/saved/page.tsx
@@ -1,14 +1,26 @@
-import React from "react";
-import Header from '@/components/Header';
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Header from "@/components/Header";
+import ImageGrid from "@/components/ImageGrid";
 
 const Saved = () => {
+  const [savedImages, setSavedImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const savedImages = JSON.parse(localStorage.getItem("savedImages") || "[]");
+    setSavedImages(savedImages);
+  }, []);
+
   return (
     <div className="text-black">
       <Header />
 
       <h2>Saved</h2>
       <br />
-      <p>In this page you can the the posts you saved</p>
+      <p>In this page you can see the posts you saved</p>
+
+      <ImageGrid images={savedImages} downloadLinks={savedImages} />
     </div>
   );
 };
--- components/WhiteFrame.tsx
+++ components/WhiteFrame.tsx
@@ -3,6 +3,7 @@ import Image, { StaticImageData } from "next/image";
 import Love from "@/public/svg/heart-svgrepo-com.png";
 import Share from "@/public/svg/share-svgrepo-com.png";
 import Saved from "@/public/svg/saved-svgrepo-com.png";
+import Savedfilled from "@/public/svg/saved-filled-svgrepo-com.png";
 import Button from "@/components/Button";
 
 interface Props {
@@ -45,6 +46,17 @@ const WhiteFrame: React.FC<Props> = ({ imageSrc }) => {
 
   const handleSaveClick = () => {
     setSavedState(!savedState);
+
+    const savedImages = JSON.parse(localStorage.getItem("savedImages") || "[]");
+    const imageUrl = typeof imageSrc === "string" ? imageSrc : imageSrc.src;
+
+    if (!savedImages.includes(imageUrl)) {
+      savedImages.push(imageUrl);
+      localStorage.setItem("savedImages", JSON.stringify(savedImages));
+      console.log("Image saved!");
+    } else {
+      console.log("Image already saved!");
+    }
   };
 
   return (
@@ -66,7 +78,7 @@ const WhiteFrame: React.FC<Props> = ({ imageSrc }) => {
         </button>
         <button onClick={handleSaveClick}>
           <Image
-            src={Saved}
+            src={savedState ? Saved : Saved}
             alt="saved"
             className="w-[30px] h-[30px] hover:scale-110 transition-all duration-150"
           />
