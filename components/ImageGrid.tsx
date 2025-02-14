5ebd0efad81fb47666540ca7b878967d7073dff1
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-02-14 23:41:39 +0330

    first step of new dynamic pages

--- components/ImageGrid.tsx
+++ components/ImageGrid.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
 import WhiteFrame from "@/components/WhiteFrame";
+import Link from "next/link";
 
 interface ImageGridProps {
   images: (string | StaticImageData)[];
@@ -13,13 +14,15 @@ const ImageGrid: React.FC<ImageGridProps> = ({ images, withIconsAndButtons = tru
     <div className="flex flex-wrap justify-center gap-5 mt-10">
       {images.map((image, index) => (
         <div key={index} className="sm:w-1/4">
-          <Image
-            src={image}
-            alt={`photo ${index}`}
-            width={500}
-            height={500}
-            className="object-cover"
-          />
+          <Link href={`/photos/${index}`} passHref> {/* Dynamic route */}
+            <Image
+              src={image}
+              alt={`photo ${index}`}
+              width={500}
+              height={500}
+              className="object-cover cursor-pointer"
+            />
+          </Link>
           {withIconsAndButtons && (
             <WhiteFrame imageSrc={image} />
           )}
