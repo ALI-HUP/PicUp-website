10609ca96e69f9f39c103e4e424c4744ce16b896
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-02-17 15:43:07 +0330

    fixing the click problem a little bit

--- components/ImageGrid.tsx
+++ components/ImageGrid.tsx
@@ -1,37 +1,28 @@
 import React from "react";
-import Image, { StaticImageData } from "next/image";
-import WhiteFrame from "@/components/WhiteFrame";
+import Image from "next/image";
 import Link from "next/link";
+import WhiteFrame from "@/components/WhiteFrame";
 
 interface ImageGridProps {
-  images: (string | StaticImageData)[];
-  downloadLinks: string[];
+  images: { id: number; src: string }[];
   withIconsAndButtons?: boolean;
 }
 
 const ImageGrid: React.FC<ImageGridProps> = ({ images, withIconsAndButtons = true }) => {
   return (
     <div className="flex flex-wrap justify-center gap-5 mt-10">
-      {images.map((image, index) => (
-        <div key={index} className="sm:w-1/4">
-          <Link
-            href={{
-              pathname: `/photos/${index}`,
-              query: { src: typeof image === "string" ? image : image.src }, // Pass the correct image source
-            }}
-            passHref
-          >
+      {images.map((image) => (
+        <div key={image.id} className="sm:w-1/4">
+          <Link href={`/photo/${image.id}`} passHref>
             <Image
-              src={image}
-              alt={`photo ${index}`}
+              src={image.src}
+              alt={`Photo ${image.id}`}
               width={500}
               height={500}
               className="object-cover cursor-pointer"
             />
           </Link>
-          {withIconsAndButtons && (
-            <WhiteFrame imageSrc={image} />
-          )}
+          {withIconsAndButtons && <WhiteFrame imageSrc={image.src} />}
         </div>
       ))}
     </div>
