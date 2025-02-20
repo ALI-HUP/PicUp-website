f2da51169e8ee5bf29345252d399af03cf792d51
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-02-20 23:40:41 +0330

    activate image grid component in home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,16 @@
 
 import React from "react";
 import Header from "../components/Header";
+import ImageGrid from "@/components/ImageGrid";
+import { useImageStore } from "@/store/imageStore";
 
 const Home = () => {
+  const images = useImageStore((state) => state.images);
+
   return (
     <div className="mb-40">
       <Header />
+      <ImageGrid images={images.map(img => ({ id: String(img.id), src: img.src }))} />
     </div>
   );
 };
