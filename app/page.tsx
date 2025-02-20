2881bc6acb826ca520f4f60218ab8b026c1501b2
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-02-20 23:06:46 +0330

    home: ready to get new design with the tags

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,11 @@
 
 import React from "react";
 import Header from "../components/Header";
-import ImageGrid from "../components/ImageGrid";
-
-const hardcodedImages = [
-  { id: 101, src: "/pics/IMG_20220307_152910_643.jpg" },
-  { id: 102, src: "/pics/wp7864479.png" },
-  { id: 103, src: "/pics/wp7937871.jpg" },
-  { id: 104, src: "/pics/IMG_20220307_152910_643.jpg" },
-];
 
 const Home = () => {
   return (
     <div className="mb-40">
       <Header />
-      <ImageGrid images={hardcodedImages} />
     </div>
   );
 };
