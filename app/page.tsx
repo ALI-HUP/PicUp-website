c22a6c0904f34ccc8ef5019d19675323ed3797fc
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-02-20 23:48:00 +0330

    image grid in home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,12 @@
 import React from "react";
 import Header from "../components/Header";
 import ImageGrid from "@/components/ImageGrid";
-import { useImageStore } from "@/store/imageStore";
 
 const Home = () => {
-  const images = useImageStore((state) => state.images);
-
   return (
     <div className="mb-40">
       <Header />
-      <ImageGrid images={images.map(img => ({ id: String(img.id), src: img.src }))} />
+      <ImageGrid images={[]} />
     </div>
   );
 };
