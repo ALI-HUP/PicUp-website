85f05deb41c648a751eda347685a601edafab2c1
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-03-10 13:07:45 +0330

    start working on tag pallets

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,40 @@
 import React from "react";
 import Header from "../components/Header";
 import ImageGrid from "@/components/ImageGrid";
+import Image from "next/image";
+import profilePic from "@/public/profile/360_F_819663119_che4sZSrmQv8uQJOzuN9TVQFQNHJlfQ2.jpg";
 
 const Home = () => {
   return (
     <div className="mb-40">
       <Header />
       <ImageGrid images={[]} />
+
+      <div className="flex justify-center items-center flex-col">
+        <div className="bg-slate-700 p-10 w-[50%] m-5 flex gap-20">
+          <div className="">
+            <p>;ogjbn;kjn</p>
+          </div>
+          <div>
+            <Image alt="profile pic" src={profilePic} className="w-40"/>
+          </div>
+          <div>
+            <Image alt="profile pic" src={profilePic} className="w-40"/>
+          </div>
+        </div>
+
+        <div className="bg-slate-700 p-10 w-[50%] m-5 flex gap-20">
+          <div className="">
+            <p>;ogjbn;kjn</p>
+          </div>
+          <div>
+            <Image alt="profile pic" src={profilePic} className="w-40"/>
+          </div>
+          <div>
+            <Image alt="profile pic" src={profilePic} className="w-40"/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
