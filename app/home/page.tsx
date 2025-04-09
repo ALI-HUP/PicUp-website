3628a45f576187f877cae71de8ac9b6f84a37ef7
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-04-09 19:18:36 +0330

    home updated

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,30 +1,26 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
-import ImageGrid from "@/components/ImageGrid";
-import Image from "next/image";
-import profilePic from "@/public/profile/360_F_819663119_che4sZSrmQv8uQJOzuN9TVQFQNHJlfQ2.jpg";
+import LogoutButton from "@/components/LogoutButton";
+
 
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    return <p className="text-center mt-10">Access Denied. Please log in.</p>;
+    return (
+      <div className="text-center mt-10">
+        <p>You are not logged in. Redirecting to login...</p>
+        <p>Please log in to access the home page.</p>
+      </div>
+    );
   }
 
   return (
     <div className="mb-40">
-      <ImageGrid images={[]} />
+      <h1>Welcome to the Home Page!</h1>
+      <p>Enjoy exploring amazing photos and galleries.</p>
+      <LogoutButton />
 
-      <div className="flex justify-center items-center flex-col">
-        <div className="bg-slate-700 p-10 w-[50%] m-5 flex gap-20">
-          <div>
-            <p>Welcome to the Home Page! Here's your profile info.</p>
-          </div>
-          <div>
-            <Image alt="profile pic" src={profilePic} className="w-40" />
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
