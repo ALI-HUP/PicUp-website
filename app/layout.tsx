35105b103cfc98db87ade9412bd1c213ac14113c
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-04-11 14:43:55 +0330

    hide header in landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header";
-
+import HeaderWrapper from "../components/HeaderWrapper"; // updated import
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Header />
+        <HeaderWrapper /> {/* Only shows header if needed */}
         {children}
       </body>
     </html>
