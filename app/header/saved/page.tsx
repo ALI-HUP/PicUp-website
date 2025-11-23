327c1e915bb5518be68045ad436bc78f8cb3987a
ALI-HUP <ali.sheikhbahaie84@gmail.com>
2025-11-23 19:21:10 +0330

    build fix

--- app/header/saved/page.tsx
+++ app/header/saved/page.tsx
@@ -1,19 +1,27 @@
 "use client";
+export const dynamic = "force-dynamic";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ImageGrid from "@/components/ImageGrid";
 
 const Saved = () => {
-  const savedImages = JSON.parse(localStorage.getItem("savedImages") || "[]");
+  const [savedImages, setSavedImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const stored = JSON.parse(localStorage.getItem("savedImages") || "[]");
+    setSavedImages(stored);
+  }, []);
 
   return (
     <div className="mb-40">
-
       <div className="bg-slate-900 flex justify-center items-center text-2xl font-extrabold p-14 text-white">
         <h1>Saved Photos: {savedImages.length}</h1>
       </div>
+
       {savedImages.length > 0 ? (
-        <ImageGrid images={savedImages.map((src: string) => ({ id: src, src }))} />
+        <ImageGrid
+          images={savedImages.map((src: string) => ({ id: src, src }))}
+        />
       ) : (
         <p className="text-center mt-10">No saved images yet...</p>
       )}
